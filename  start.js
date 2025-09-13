3e2ff9454dcbe7995d106a0aaf69c48dcb4d1aab
Akiva Altman <akivaaltman@Akivas-MacBook-Pro.local>
2025-09-14 00:03:20 +0300

    Initial commit - NextBet7 WhatsApp Bot

---
+++  start.js
@@ -0,0 +1,17 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2822
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\froman\fcharset0 Times-Roman;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11220\viewh8100\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs24 \cf0 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 // start.js - Entry point for Render deployment\
+const NextBet7GreenAPIBot = require('./bot.js');\
+\
+console.log('\uc0\u55357 \u56960  Starting NextBet7 Bot on Render...');\
+console.log('\uc0\u55357 \u56561  Instance ID:', process.env.GREEN_API_INSTANCE_ID);\
+console.log('\uc0\u55356 \u57104  Environment: Production');\
+\
+// The bot will start automatically when required}
